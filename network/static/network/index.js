1a283f27d5f9fb41d0e97c2106d47f13819dc0fc
FEVRIER <tony.fevrier62@gmail.com>
2024-09-09 12:28:38 -0400

    feat(index.html): replace a links by button links to make a single page application

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector("#allposts-content").addEventListener("click", print_all_posts);
+    document.querySelector("#profile-content").addEventListener("click", print_profile);
+    document.querySelector("#following-content").addEventListener("click", print_following_posts);
+}); 
 
-});
+
+function print_all_posts(){
+    document.querySelector("#allposts-content")
+}
+
+
+function print_profile(){
+    document.querySelector("#profile-content")
+}
+
+
+function print_following_posts(){
+    document.querySelector("#following-content")
+}
