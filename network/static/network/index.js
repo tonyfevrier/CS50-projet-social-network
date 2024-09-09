1c3c2d47e1e4b0b6209e6a9e12abfaae0a2e04ac
FEVRIER <tony.fevrier62@gmail.com>
2024-09-09 12:32:36 -0400

    feat(index.js): write the three functions to load the pages of All posts, profile and following

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,20 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(){
-    document.querySelector("#allposts-content").addEventListener("click", print_all_posts);
-    document.querySelector("#profile-content").addEventListener("click", print_profile);
-    document.querySelector("#following-content").addEventListener("click", print_following_posts);
+    console.log( document.querySelector("#allposts-content"))
+    document.querySelector("#allposts-btn").addEventListener("click", print_all_posts);
+    document.querySelector("#profile-btn").addEventListener("click", print_profile);
+    document.querySelector("#following-btn").addEventListener("click", print_following_posts);
 }); 
 
 
-function print_all_posts(){
-    document.querySelector("#allposts-content")
+function print_all_posts(event){
+    document.querySelector("#allposts-content").style.display = "block";
+    document.querySelector("#profile-content").style.display ="none";
+    document.querySelector("#following-content").style.display = "none";
 }
 
 
 function print_profile(){
-    document.querySelector("#profile-content")
+    document.querySelector("#allposts-content").style.display = "none";
+    document.querySelector("#profile-content").style.display = "block";
+    document.querySelector("#following-content").style.display = "none";
 }
 
 
 function print_following_posts(){
-    document.querySelector("#following-content")
+    document.querySelector("#allposts-content").style.display = "none";
+    document.querySelector("#profile-content").style.display ="none";
+    document.querySelector("#following-content").style.display = "block";
 }
